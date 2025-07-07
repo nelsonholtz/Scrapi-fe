2c707f97b46dfef0f3fd6129888bd0b4e3e1bc56
LeroyHJ <leroy_jallow_@hotmail.co.uk>
2025-07-07 12:18:10 +0100

    adding test image in progress

---
+++ src/components/DraggableImage.jsx
@@ -0,0 +1,9 @@
+import { Stage, Layer, Image } from 'react-konva'
+import useImage from 'use-image'
+
+const DraggableImage = () => {
+    const [image] = useImage('https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg')
+
+}
+
+export default DraggableImage
